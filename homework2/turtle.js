3302886ab0edfba96b2ff589e4aafd8b3f60a91d
Erika Balbino <erk.balbino@gmail.com>
2018-06-13 09:28:10 -0700

    Start Turtle graphic

---
+++ homework2/turtle.js
@@ -0,0 +1,23 @@
+class Turtle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    forward(n) {
+
+    }
+
+    right(){
+
+    }
+
+    left(){
+
+    }
+
+    print(){
+        
+    }
+
+}

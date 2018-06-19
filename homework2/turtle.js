3479cacb29264a15ec65a417def7344051901746
Erika Balbino <erk.balbino@gmail.com>
2018-06-19 16:18:15 -0700

    working on solution

--- homework2/turtle.js
+++ homework2/turtle.js
@@ -1,10 +1,23 @@
 class Turtle {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+    constructor(col, row) {
+        this.col = col;
+        this.row = row;
+        this.newGrid = []
+        let array1 = [];
+        let array2 = [];
+
+      
+        // const newGrid = [...Array(num)].map(e => [...Array(num)].map(e => ' '))
+        // let initialPosition = [this.col, this.row]
     }
 
     forward(n) {
+        this.newGrid[this.col, this.row] = "*";
+        // let newCol = this.col;
+        for (let i = 1; i < n;  i++) {
+            this.newGrid[i, this.row] += "*";
+        }
+        console.log(this.newGrid);
 
     }
 
@@ -16,8 +29,35 @@ class Turtle {
 
     }
 
+    allPoints(){
+
+    }
+
     print(){
-        
+        function newArray(array1, array2) {
+            for (let a = 0; a < array1.lenght; a++ ) {
+                for (let b = 0; b < array2; b++){
+                    newGrid.push([array1[a], array2[b]])
+                }
+            }
+            return newGrid;
+        }
     }
 
 }
+
+new Turtle(0, 4)
+  .forward(3)
+//   .left()
+//   .forward(3)
+//   .right()
+//   .forward(5)
+//   .right()
+//   .forward(8)
+//   .right()
+//   .forward(5)
+//   .right()
+//   .forward(3)
+//   .left()
+//   .forward(3)
+//   .print();

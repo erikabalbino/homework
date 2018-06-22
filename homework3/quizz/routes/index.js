46742aa9945958ba9c699486223f648c9054fa0e
Erika Balbino <erk.balbino@gmail.com>
2018-06-21 23:34:33 -0700

    Built more questions for quizz and calculate percentual and grade on index.js, fix cookies for result.ejs

--- homework3/quizz/routes/index.js
+++ homework3/quizz/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render("welcome");
 });
 
-const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7; // uma semana
+const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7; // one week
 
 router.post('/name', (req, res) => {
   const name = req.body.name;
@@ -27,31 +27,63 @@ router.get('/quiz', (req, res) => {
 })
 
 router.post('/quiz', (req, res) => {
-  // res.json(req.body.question2);
+  // res.json(req.body);
   const resQuestion1 = req.body.question1;
   const resQuestion2 = req.body.question2;
+  let resQuestion3 = req.body.question3;
+  let resQuestion4 = req.body.question4;
 
   let results = 0.0
   if (resQuestion1 == 1) {
     results += 1
-  }
+  };
   if (resQuestion2 == 3) {
-    results += 1
-  }
+    results += 1 
+  };
+  if (resQuestion3[0] == 1 && resQuestion3[1] == 2) {
+    results += 1 
+  };
+  if (resQuestion4[0] == 1 && resQuestion4[1] == 4) {
+    results += 1 
+  };
 
-  const percent = results/2*100;
-  //-> Do the grade calc here
-  const grade = "A"
-  // res.send(rescalc.toString());
-  // res.send(`You scored ${rescalc}%, you got ${results} out of 2 questions`)
-  const returnResults = {
+  // console.log("results:", results);
+  
+  const percent = results/4*100;
+  let grade = "";
+  let greeting = "";
+
+  console.log("percent:", percent);
+
+  switch (true) {
+    case  (percent >= 80 && percent <= 100):
+        grade = "A";
+        greeting = "👏 Congratulations! You did a great job! ☕️ ☕️ ☕️ ☕️ ☕️";
+        break;
+    case (percent >= 68 && percent <= 79):
+        grade = "B";
+        break;
+    case (percent >= 55 && percent <= 67):
+        grade = "C";
+        break;
+    case (percent >= 50 && percent <= 54):
+        grade = "D";
+        break;
+    case (percent >= 20 && percent <= 49):
+        grade = "E";
+        break;
+    default:
+        grade = "F";
+}
+
+const returnResults = {
     percent,
     grade, 
-    results
+    results,
+    greeting
   }
 
   res.cookie("cookieResults", returnResults, {maxAge: COOKIE_MAX_AGE})
-  // res.cookie("results", rescalc, {maxAge: COOKIE_MAX_AGE})
 
   res.redirect("/quiz/results")
 

fc8ff4c75fcb39a65ee702654471e9c2d672f299
Erika Balbino <erk.balbino@gmail.com>
2018-06-26 20:05:16 -0700

    Build New page

--- homework4/Super_Team_Picker/routes/index.js
+++ homework4/Super_Team_Picker/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('home');
 });
+
+router.get('/new', (req,res) => {
+  res.render('new')
+})
+
 
 module.exports = router;

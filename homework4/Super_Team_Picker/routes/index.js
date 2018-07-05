63f0c68963af74284270dee98067bfa68579e37e
Erika Balbino <erk.balbino@gmail.com>
2018-07-04 19:38:18 -0700

    Create project

--- homework4/Super_Team_Picker/routes/index.js
+++ homework4/Super_Team_Picker/routes/index.js
@@ -51,13 +51,42 @@ router.get('/:id', (req, res) => {
   .where({ id: req.params.id }) 
   .then(results => {
     const [cohort] = results;
+
+    let members_arr  = cohort.members.split(',').map(x => x.trim());
+
+    // console.log(members_arr);
+
+    let j = Math.floor(Math.random() * members_arr.length);
+    for (let i = 0; i < members_arr.length; i++) {
+          temp = members_arr[i];
+          members_arr[i] = members_arr[j];
+          members_arr[j] = temp;
+      }
+    // console.log(members_arr);
+
+    
+    let new_team = [];
+    let new_length = 0;
+
+    if (opt === "2"){
+        while(members_arr.length) {new_team.push(members_arr.splice(0,qty))}
+    } else {
+        let new_length = Math.ceil(members_arr.length / qty)
+        // console.log(new_length)
+        // console.log(qty)
+        // console.log(members_arr.length)
+        // console.log(members_arr)
+        for(let j= 0; j < members_arr.length; j+=new_length){
+          console.log(j);
+          new_team.push(members_arr.slice(j, j + new_length));
+        }
+        // while(new_length <= members_arr.length) { new_team.push(members_arr.splice(0,new_length)) }
+    }
+
     // res.send(cohort);
-    res.render("cohort", { cohort: cohort });
-    console.log(cohort, opt, qty);
+    res.render("cohort", {cohort,new_team, qty, opt});
+    // console.log(cohort, new_team, opt, qty);
   });
-
-
-
 })
 
 

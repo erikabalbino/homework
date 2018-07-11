a0606e06ac4cfac53c17cf7f9dcf51941d45c858
Erika Balbino <erk.balbino@gmail.com>
2018-07-10 20:52:43 -0700

    Add update action on User controller

--- homework5/BlogOnRails/app/controllers/users_controller.rb
+++ homework5/BlogOnRails/app/controllers/users_controller.rb
@@ -20,6 +20,17 @@ class UsersController < ApplicationController
         @user = User.find params[:id]
     end
 
+    def update
+        # render json: params
+
+        if current_user.update_attributes(params.require(:user).permit(:first_name, :email))
+            redirect_to posts_path
+        else
+            render :edit
+        end
+        
+    end
+    
     private
     def user_params 
         params.require(:user).permit(
